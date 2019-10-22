886e89791f857a85366c47d8af02acc4dab1b8d8
Riquelme Ciro <ciroriquelme05@gmail.com>
2019-10-22 11:31:45 -0300

    Interface Alta titular

---
+++ src/main/java/Interfaces/Interfaz_Alta_Titular.java
@@ -0,0 +1,19 @@
+package Interfaces;
+
+import javax.swing.*;
+
+public class Interfaz_Alta_Titular {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JComboBox comboBox1;
+    private JTextArea XXXXXXXXXTextArea;
+    private JButton confirmarButton;
+    private JButton cancelarButton;
+    private JPanel panel1;
+    private JButton buscarDatosButton;
+    private JComboBox comboBox2;
+    private JCheckBox esDonanteCheckBox;
+}
---
+++ src/main/java/Interfaces/Interfaz_Licencias_Expiradas.java
@@ -0,0 +1,24 @@
+package Interfaces;
+
+import javax.swing.*;
+
+public class Interfaz_Licencias_Expiradas {
+    private JPanel panel1;
+    private JTextField tf_dni;
+    private JTextField tf_nombre;
+    private JTextField tf_apellido;
+    private JCheckBox aCheckBox;
+    private JCheckBox bCheckBox;
+    private JCheckBox cCheckBox;
+    private JCheckBox dCheckBox;
+    private JCheckBox eCheckBox;
+    private JCheckBox fCheckBox;
+    private JCheckBox gCheckBox;
+    private JTextField tf_desde;
+    private JTextField tf_hasta;
+    private JTable table_resultados;
+    private JButton volverButton;
+    private JButton buscarButton;
+    private JButton renovarButton;
+    private JScrollPane scrollPane;
+}
