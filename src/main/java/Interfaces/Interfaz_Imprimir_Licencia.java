afe525f65e125b6130cf1375194ae4d2fa40f78a
Riquelme Ciro <ciroriquelme05@gmail.com>
2019-10-22 00:05:57 -0300

    Interface imprimir licencia

--- src/main/java/Interfaces/Interfaz_Imprimir_Licencia.java
+++ src/main/java/Interfaces/Interfaz_Imprimir_Licencia.java
@@ -4,4 +4,20 @@ import javax.swing.*;
 
 public class Interfaz_Imprimir_Licencia {
     private JPanel panel1;
+    private JTextField tf_dni;
+    private JTextField tf_nombre;
+    private JTextField tf_apellido;
+    private JCheckBox aCheckBox;
+    private JCheckBox bCheckBox;
+    private JCheckBox cCheckBox;
+    private JCheckBox dCheckBox;
+    private JCheckBox eCheckBox;
+    private JCheckBox fCheckBox;
+    private JCheckBox gCheckBox;
+    private JTextField tf_desde;
+    private JTextField tf_hasta;
+    private JTable table_resultados;
+    private JButton cancelarButton;
+    private JButton buscarButton;
+    private JButton imprimirButton;
 }
