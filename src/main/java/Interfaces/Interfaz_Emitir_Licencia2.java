1b3e6bcf462ab5616f645f27c21e187cf263a3a8
Riquelme Ciro <ciroriquelme05@gmail.com>
2019-10-21 23:11:38 -0300

    Interface Emitir licencia 2

---
+++ src/main/java/Interfaces/Interfaz_Emitir_Licencia2.java
@@ -0,0 +1,27 @@
+package Interfaces;
+
+import javax.swing.*;
+
+public class Interfaz_Emitir_Licencia2 {
+
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JCheckBox esDonanteCheckBox;
+    private JComboBox comboBox1;
+    private JTextArea XXXXXXXXXTextArea;
+    private JCheckBox aCheckBox;
+    private JCheckBox bCheckBox;
+    private JCheckBox cCheckBox;
+    private JCheckBox dCheckBox;
+    private JCheckBox eCheckBox;
+    private JCheckBox fCheckBox;
+    private JCheckBox gCheckBox;
+    private JTextField DDMMAATextField;
+    private JTextField DDMMAATextField1;
+    private JButton confirmarButton;
+    private JButton cancelarButton;
+    private JPanel panel1;
+}
--- src/main/java/Interfaces/Interfaz_Imprimir_Licencia.java
+++ src/main/java/Interfaces/Interfaz_Imprimir_Licencia.java
@@ -1,3 +1,5 @@
+package Interfaces;
+
 import javax.swing.*;
 
 public class Interfaz_Imprimir_Licencia {
