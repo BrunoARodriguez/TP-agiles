a95389fc61a86b1f055b052900ccb4b1b139b69e
HerniiGoH <calbornoz@frsf.utn.edu.ar>
2019-10-25 16:59:41 -0300

    cambios antes de merge

--- src/main/java/Interfaces/Interfaz_Alta_Titular.java
+++ src/main/java/Interfaces/Interfaz_Alta_Titular.java
@@ -1,21 +1,40 @@
 package Interfaces;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Interfaz_Alta_Titular {
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
-    private JComboBox comboBox1;
-    private JTextArea XXXXXXXXXTextArea;
+    private JTextField tfNumeroDocumento;
+    private JTextField tfApellido;
+    private JTextField tfNombre;
+    private JTextField tfFDeNac;
+    private JTextField tfDomicilio;
+    private JComboBox cbTipoSangre;
+    private JTextArea taObservaciones;
     private JButton confirmarButton;
     private JButton cancelarButton;
     private JPanel rootPane;
     private JButton buscarDatosButton;
-    private JComboBox comboBox2;
     private JCheckBox esDonanteCheckBox;
+
+    public Interfaz_Alta_Titular() {
+        buscarDatosButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String sNroDocumento=tfNumeroDocumento.getText();
+                Integer nroDocumento;
+                if(sNroDocumento != null){
+                    Integer.valueOf(sNroDocumento);
+                }
+
+                //acá tenemos que buscar nroDocumento enla base de datos y traer toda la info
+
+                
+
+            }
+        });
+    }
 
     public JPanel getPane(){
         return rootPane;
